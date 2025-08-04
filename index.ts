94e863a24241c5f9d839bef972f3d48401675af9
Stefano Casasola <ste.casasola@gmail.com>
2025-08-04 09:56:38 +0200

    Working TS template

---
+++ index.ts
@@ -0,0 +1,12 @@
+// questo è un file TypeScript!
+// i commenti si fanno sempre con //
+
+// in un file TS si può fare TUTTO QUELLO che già sapete fare in un file JS
+console.log('Ciao, sono TypeScript!')
+const x = 'Stefano'
+// TS è 100% "retrocompatibile" con JS
+// però potremo fare delle cose che NON si possono fare in JS!
+// potremo ESPANDERE questa sintassi
+console.log('questo è un altro console log!')
+
+console.log('ultimo messaggio ancora una voltaaaa')
